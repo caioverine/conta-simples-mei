63b6c57d99d1b2842eb9da9d58cc1ec59bc6c135
Caio Cabral <viniciuscaio.17@gmail.com>
2025-05-14 22:53:56 -0300

    Configuração inicial

---
+++ backend/src/main/kotlin/com/contasimplesmei/ContaSimplesMeiApplication.kt
@@ -0,0 +1,11 @@
+package com.contasimplesmei
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContaSimplesMeiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContaSimplesMeiApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/contasimplesmei/ContaSimplesMeiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.contasimplesmei
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContaSimplesMeiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
