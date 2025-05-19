91b46843097ef7e7602b15942df0e67933a5ea44
DESKTOP-8A4HGFL\caioverine <viniciuscaio.17@gmail.com>
2025-05-19 18:49:21 -0300

    Correção no módulo de usuário

--- backend/src/main/kotlin/com/contasimplesmei/model/Usuario.kt
+++ backend/src/main/kotlin/com/contasimplesmei/model/Usuario.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.UUID
@@ -13,6 +14,7 @@ import java.util.UUID
 @Table(name = "usuarios")
 data class Usuario(
     @Id
+    @GeneratedValue
     val id: UUID? = null,
 
     @Column(nullable = false)
