799467a67cb1f7b334111a2abd7dd217ed99b281
DESKTOP-8A4HGFL\caioverine <viniciuscaio.17@gmail.com>
2025-05-23 10:11:57 -0300

    Estilização da tela de Login com Tailwind

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -28,36 +28,49 @@ export default function Login() {
     };
 
     return (
-    <div style={{ maxWidth: 400, margin: "100px auto", textAlign: "center" }}>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div style={{ marginBottom: 10 }}>
-          <input
-            type="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={{ width: "100%", padding: 8 }}
-          />
+    <div className="flex items-center justify-center min-h-screen px-4">
+      <div className="w-full max-w-xl p-8">
+        <div className="flex-1">
+          <div className="text-center">
+            <div className="flex justify-center mb-4">
+              <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt=""/>
+            </div>
+
+            <p className="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+          </div>
+
+          <div className="mt-8">
+            <form onSubmit={handleLogin}>
+              <div>
+                <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                <input type="email" name="email" id="email" placeholder="example@example.com" value={email} onChange={(e) => setEmail(e.target.value)}
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
+
+              <div className="mt-6">
+                <div className="flex justify-between mb-2">
+                  <label className="text-sm text-gray-600 dark:text-gray-200">Password</label>
+                  <a href="#" className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
+                </div>
+
+                <input type="password" name="password" id="password" placeholder="Your Password" value={senha} onChange={(e) => setSenha(e.target.value)}
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+              </div>
+
+              <div className="mt-6">
+                <button type="submit" 
+                  className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                  Sign in
+                </button>
+              </div>
+
+            </form>
+
+            <p className="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#" 
+              className="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
+          </div>
         </div>
-        <div style={{ marginBottom: 10 }}>
-          <input
-            type="password"
-            placeholder="Senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            style={{ width: "100%", padding: 8 }}
-          />
-        </div>
-        <button type="submit" style={{ padding: "8px 16px" }}>
-          Entrar
-        </button>
-          <div className="h-screen flex items-center justify-center bg-gray-100">
-        <button className="bg-blue-600 text-white font-bold px-6 py-2 rounded hover:bg-blue-700">
-          Testar Tailwind
-        </button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
