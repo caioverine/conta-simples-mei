da09bb24c6f2c6dcb75b73f43811a5f85f0f80c9
DESKTOP-8A4HGFL\caioverine <viniciuscaio.17@gmail.com>
2025-05-23 10:11:16 -0300

    Correção de espaçamento

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,16 +2,16 @@
 export default function Dashboard() {
 
   return (
-      <div className="container px-6 py-16 mx-auto text-center">
-        <div className="max-w-lg mx-auto">
-          <h2 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
-            Dashboard
-          </h2>
+    <div className="container px-6 py-16 mx-auto text-center">
+      <div className="max-w-lg mx-auto">
+        <h2 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
+          Dashboard
+        </h2>
 
-        </div>
-        <div className="flex justify-center mt-10">
-          <p>Bem-vindo ao sistema de gestão financeira para MEIs!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>
-        </div>
       </div>
+      <div className="flex justify-center mt-10">
+        <p>Bem-vindo ao sistema de gestão financeira para MEIs!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>
+      </div>
+    </div>
   );
 }
