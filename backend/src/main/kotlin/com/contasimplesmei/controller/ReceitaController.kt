eab47ba2b2b65fc7f798119fa938d929044c7415
DESKTOP-8A4HGFL\caioverine <viniciuscaio.17@gmail.com>
2025-06-13 17:11:40 -0300

    Correção de imports

--- backend/src/main/kotlin/com/contasimplesmei/controller/ReceitaController.kt
+++ backend/src/main/kotlin/com/contasimplesmei/controller/ReceitaController.kt
@@ -3,23 +3,13 @@ package com.contasimplesmei.controller
 import com.contasimplesmei.dto.ReceitaRequestDTO
 import com.contasimplesmei.dto.ReceitaResponseDTO
 import com.contasimplesmei.mapper.toResponseDTO
-import com.contasimplesmei.model.Receita
 import com.contasimplesmei.service.ReceitaService
 import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import java.net.URI
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/receitas")
